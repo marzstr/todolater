a9d1966d2df30e5c0d2b32409f8fa3e91813c9ed
marzstr <maria@dm3sol.com>
2015-02-18 18:48:28 -0800

    Added documentation, changed design

---
+++ Readme.txt
@@ -0,0 +1,35 @@
+Today / Tomorrow Documetation
+by mstrnal
+january 2015
+
+-------------------------------------------------
+
+Today / Tomorrow allows user to document and prioritize the items they want to accomplish, as well as be able to see the history of all completed items. Having the ability to sort through items and push the items you know you will not be able to complete today, to tomorrow. All uncompleted items default to today's list, therefore the user will constantly be able to arrage their priorities for that day. Having a historical record of completed items, allows for quick scan of when tasks were completed. 
+
+-------------------------------------------------- 
+
+Each item has: 
+- id
+- name
+- note 
+- date created 
+- date tobecompleted 
+- date completed 
+- is completed
+- order
+- active 
+
+__________________________________________________
+
+Included files in project: 
+
+- todos.html
+- todos.js
+- todos.css (compiled from todos.scss)
+
+
+____________________________________________________
+
+Prototype deplayed to : https://todolater.meteor.com
+
+
--- todos.js
+++ todos.js
@@ -4,11 +4,24 @@ Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("todos");
   
   Template.body.helpers({
     todos:  function () {
-      return Todos.find({}, {sort: { createdAt: -1 }});
-    }
+
+      if(Session.get("hideCompleted")){
+        return Todos.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Todos.find({}, {sort: { createdAt: -1 }});
+      }
+      
+    }, 
+    hideCompleted: function (){
+      return Session.get("hideCompleted");
+    }, 
+    incompleteCount:function () {
+      return Todos.find({checked: {$ne: true}}).count();
+    }, 
       
   });
 
@@ -20,29 +33,104 @@ if (Meteor.isClient) {
       //var text = event.target.text.value;
       var text = $('#new_task_text').val();
 
-      console.log(text);
-
-      Todos.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call("addItem", text);
 
       //event.target.text.value = "";
       $('#new_task_text').val('');
 
       return false; // must return false to prevent default form submmit
+    }, 
+
+    'change .hide-completed input': function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.item.events({
     // Event: complete task
     'click .toggle-checked': function () {
-      Todos.update(this._id, {$set: {checked: ! this.checked }});
+      Meteor.call("setChecked", this._id, !this.checked);
     },
 
     'click .delete': function (){
-      Todos.remove(this._id);
+      Meteor.call("removeItem", this._id);
+    }, 
+
+    'click .toggle-private': function () {
+      Meteor.call("setPrivate", this._id,  ! this.private);
     }
   });
-  
+
+  Template.item.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
+
+if(Meteor.isServer){
+  Meteor.publish('todos', function () {
+    return Todos.find({
+      $or: [
+        {private: {$ne: true}},
+        {owner: this.userId }
+      ]
+    });
+  });
+}
+
+Meteor.methods({
+  addItem: function (text) {
+    if(!Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Todos.insert({
+        text: text,
+        createdAt: new Date(), 
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      });
+  }, 
+
+  removeItem: function (itemId){
+    var item = Todos.findOne(taskId);
+
+    // Validation check
+    if(item.private && item.owner !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Todos.remove(itemId);
+  }, 
+
+  setChecked: function (itemId, setChecked){
+    var item = Todos.findOne(taskId);
+    
+    // Validation check
+    if(item.private && item.owner !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Todos.update(itemId, {$set: {checked: setChecked }});
+  }, 
+
+  setPrivate: function (itemId, setToPrivate){
+    var item = Todos.findOne(itemId);
+
+    if(task.owner !== Meteor.userId()){
+      throw new Meteor.Error('not-authorized');
+    } 
+
+    Todos.update(itemId, { $set: { private: setToPrivate }});
+  }
+});
+
+
+
+
